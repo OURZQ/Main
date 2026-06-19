9cbb8907a8a852584ea3374e22a95fa0b9a9e61a
OURZQ <imthereal.yan@gmail.com>
2026-06-19 20:07:56 +0800

    Update All.lua

--- All.lua
+++ All.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OURZQ/Main/main/loader.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OURZQ/Main/refs/heads/main/loader.lua", true))()
